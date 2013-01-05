7091a379a7c51a9ac4c16ce0035c9d01c9640123
Rafael Paulino <rafaclasses@gmail.com>
2013-01-05 17:06:13 -0200

    Hello world

--- HelloWorldSimulator/PrincipalViewController.h
+++ HelloWorldSimulator/PrincipalViewController.h
@@ -10,4 +10,13 @@
 
 @interface PrincipalViewController : UIViewController
 
+//IBOutlet
+//e uma maneira de fazer uma conexao entre um ponteiro declarado via codigo e um componente visual posicionado via storyboard (interfacebuilder)
+
+@property (nonatomic, weak) IBOutlet UILabel *minhaOutraLabel;
+
+//IBAction
+//conexao entre um evento recebido pelo componente visual e um metodo a ser executado quando este evento ocorrer
+-(IBAction)botaoClicado:(id)sender;
+
 @end
--- HelloWorldSimulator/PrincipalViewController.m
+++ HelloWorldSimulator/PrincipalViewController.m
@@ -14,10 +14,37 @@
 
 @implementation PrincipalViewController
 
+//fazemos o @synthesize para cada property declarada no .h
+//@synthesize minhaOutraLabel = _minhaOutraLabel;
+
+-(IBAction)botaoClicado:(id)sender
+{
+    _minhaOutraLabel.text = @"Hola Mundo!";
+    
+}
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //adicionar uma label na tela com o texto hello world
+    //ponteiro para uma lab el
+    UILabel *minhaLabel;
+    
+    //alocar objetos, chamamos o metodo init da classe apos o alloc (alloc/init)
+    minhaLabel = [[UILabel alloc] init];
+    
+    //configurar a label
+    //onde ela vai aparecer
+    minhaLabel.frame = CGRectMake(20, 20, 200, 80);
+    //qual texto ela vai exibir
+    minhaLabel.text = @"Hello World"; //@""define uma string
+    //para adcionar a label na tela, mandamos uma mensagem para a view que ira receber este objeto
+    [self.view addSubview:minhaLabel];
+    
 }
 
 - (void)viewDidUnload
